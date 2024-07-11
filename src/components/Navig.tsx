4bd5e1d1590dd192e564fcf48d0ace06500cd53e
Slobodan Ludoški <161731010+ludoskis@users.noreply.github.com>
2024-07-11 23:31:42 +0200

    Kreirana navigacija

---
+++ src/components/Navig.tsx
@@ -0,0 +1,17 @@
+function Navig() {
+    return (
+        <div className="navigacija">
+            <div className="left">
+                <h1>BIBLIOTEKA</h1>
+            </div>
+            <div className="right">
+                <a href="#">Kontakt</a>
+                <a href="#">Knjige</a>
+                <a href="#">Prijavi se</a>
+                <button>Registruj se</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navig;
